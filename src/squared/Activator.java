bc51a5faf069540938ea4b336fdcb136f6438f49
Pawel Piatkowski <piatkowskip@gmail.com>
2010-06-29 21:22:43 +0200

    [FIX] database lock is released on plugin stop

--- src/squared/Activator.java
+++ src/squared/Activator.java
@@ -35,6 +35,8 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+		DBConnection.getInstance().close();
+		
 		plugin = null;
 		super.stop(context);
 	}
